ab39b92f55e3e46e4b8c5d464103541beebd2ec8
Aniket Jain <aniketjain8441@gmail.com>
2023-01-18 00:22:03 +0530

    add basic structure of the repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import json2md from "json2md";
 
-function App() {
+const App = () => {
+  // set the file name by the user and generate the markdown file of the JSON File.
+  const createMarkdownFile = (fileName) => {
+    const data = require(`./data/${fileName}.json`);
+    console.log(data);
+    const dataFile = json2md(data);
+
+    // create the Element and generate the Markdown file using Element.
+    let hiddenElement = document.createElement("a");
+    hiddenElement.download = `${fileName}.md`;
+    let blob = new Blob([dataFile], {
+      type: "text/plain",
+    });
+    hiddenElement.href = window.URL.createObjectURL(blob);
+    hiddenElement.click();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <p className="items">
+        <div className="item" onClick={() => createMarkdownFile("file1")}>
+          <h3>Heading</h3>
+          <h5>File 1</h5>
+        </div>
+        <div className="item" onClick={() => createMarkdownFile("file2")}>
+          <h3>Blockquote</h3>
+          <h5>File 2</h5>
+        </div>
+      </p>
     </div>
   );
-}
+};
 
 export default App;
